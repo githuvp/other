1d882fed357412dcf19ef9366a1c4229576c3448
githuvp <githuv@protonmail.com>
2018-03-14 23:58:27 -0400

    Other comment line

--- com/example/JavaSETest.java
+++ com/example/JavaSETest.java
@@ -12,6 +12,7 @@ public class JavaSETest {
 		elements.add(1000);
 		elements.add(100); /// Simple commentssssss
 		// This comment is only in branch-10 on local, just after push will be on remote
+		// Now there is another comment added
 		int firstElmnt = elements.get(1);
 		System.out.println("now this will print some number as: " + firstElmnt);
 	}
