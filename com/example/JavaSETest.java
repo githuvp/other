fd4c1f843d0cbf7a769264c9673c21940cbaf849
githuvp <githuv@protonmail.com>
2018-03-14 23:56:29 -0400

    added some comments to the branch-10

--- com/example/JavaSETest.java
+++ com/example/JavaSETest.java
@@ -11,6 +11,7 @@ public class JavaSETest {
 		elements.add(10);
 		elements.add(1000);
 		elements.add(100); /// Simple commentssssss
+		// This comment is only in branch-10 on local, just after push will be on remote
 		int firstElmnt = elements.get(1);
 		System.out.println("now this will print some number as: " + firstElmnt);
 	}
